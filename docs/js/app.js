3f8321a51f79b1bec2a8c564e18736fcf4486c8d
Wiput Pootong <wiput.pootong@gmail.com>
2018-04-20 03:02:02 +0700

    :sparkles: Finish header

---
+++ docs/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= $(window).height()) {
+      // Scroll under header
+    } else {
+      // At header
+    }
+  });
+});
