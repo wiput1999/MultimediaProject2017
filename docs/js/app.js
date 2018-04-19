a7369c080ed8feb18483e101c6bf45b2ffadbe56
Wiput Pootong <wiput.pootong@gmail.com>
2018-04-20 03:06:45 +0700

    :art: Improve performance~

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if ($(this).scrollTop() >= $(window).height()) {
       // Scroll under header
+      $('.video').hide();
     } else {
       // At header
+      $('.video').show();
     }
   });
 });
